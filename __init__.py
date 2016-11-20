61c1fcefda3f006f944877db7d7cc5da42db982a
LAGx <lag.x@yandex.com>
2016-11-20 20:57:32 +0200

    adding colision detection

--- __init__.py
+++ __init__.py
@@ -2,17 +2,19 @@ import pygame
 import graphicCore.window
 from coordinate.glob import *
 import phisic.vector
-
 pygame.init()
+pygame.font.init()
 
 window = graphicCore.window.createWindow()
 canvas = pygame.Surface(config.currScreenSize())
+dialog = pygame.Surface((config.currDialogSize("x"),config.currDialogSize("y")))
 
 filter = GlobalCord()
 speed = 0.4
 x = 0
 y = 0
 drug = 0.04
+alpha = 0
 
 main_vec = phisic.vector.Vector()
 up_vec = phisic.vector.Vector()
@@ -20,34 +22,46 @@ right_vec = phisic.vector.Vector()
 drug_vec = phisic.vector.Vector()
 up_vec.changeYEx(speed)
 right_vec.changeXEx(speed)
-
+player = pygame.Rect(filter.gloX(0), filter.gloY(0), 10, 10)
+house = pygame.Rect(300, 250, 100, 150)
+ruby = pygame.Rect(filter.gloX(0), filter.gloY(0), 3, 3)
 
 pygame.key.set_repeat(1,1)
 clock = pygame.time.Clock()
 pygame.mouse.set_visible(False)
 gameExit = False
 
+font = pygame.font.Font(None, 30)
+your_house = font.render("It`s your house, master.", 1, (100, 255, 200))
+your_ruby = font.render("Ruby!!!!", 1, (100, 255, 200))
+textpos1 = your_house.get_rect()
+textpos2 = your_ruby.get_rect()
+textpos1.centerx = dialog.get_rect().centerx
+textpos2.centerx = dialog.get_rect().centerx
+
 while not gameExit:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                gameExit = True
-            if event.key == pygame.K_a:
-                main_vec.changeXPlus(-right_vec.posX)
-            if event.key == pygame.K_d:
-                main_vec.changeXPlus(right_vec.posX)
-            if event.key == pygame.K_w:
-                main_vec.changeYPlus(-up_vec.posY)
-            if event.key == pygame.K_s:
-                main_vec.changeYPlus(up_vec.posY)
-            if event.key == pygame.K_f:
-                x = 0
-                y = 0
-                main_vec.posX = 0
-                main_vec.posY = 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            gameExit = True
+        if keys[pygame.K_a]:
+            main_vec.changeXPlus(-right_vec.posX)
+        if keys[pygame.K_d]:
+            main_vec.changeXPlus(right_vec.posX)
+        if keys[pygame.K_w]:
+            main_vec.changeYPlus(up_vec.posY)
+        if keys[pygame.K_s]:
+            main_vec.changeYPlus(-up_vec.posY)
+        if keys[pygame.K_f]:
+            x = 0
+            y = 0
+            main_vec.posX = 0
+            main_vec.posY = 0
+
+
 
     drug_vec.changeXEx(main_vec.posX)
     drug_vec.changeYEx(main_vec.posY)
@@ -55,18 +69,33 @@ while not gameExit:
     drug_vec.multiply(drug)
     drug_vec.invert()
     main_vec.plus(drug_vec)
-
+    player.move_ip(main_vec.posX, main_vec.posY)
     x += main_vec.posX
     y += main_vec.posY
-    print("X: ", drug_vec.posX, "            Y: ", drug_vec.posY)
 
     canvas.fill(config.background)
+    dialog.fill((10, 30,30))
 ####
-    pygame.draw.rect(canvas, config.lights_1, [int(filter.gloX(x-5)), int(filter.gloY(y+5)), 10, 10])
-    pygame.draw.circle(canvas,config.lights_2, [int(filter.gloX(0)), int(filter.gloY(0))], 2)
-    main_vec.draw(canvas, filter, x, y, 10)
+    pygame.draw.rect(canvas, (5,30,30), house)
+    player.move_ip(main_vec.posX, main_vec.posY)
+    pygame.draw.rect(canvas, config.lights_1, player)
+    pygame.draw.rect(canvas,config.lights_2, ruby)
 ####
+    dialog.set_alpha(alpha, 1)
+    if player.colliderect(house):
+        if alpha < 120:
+            alpha += 30
+        dialog.blit(your_house, (textpos1[0], textpos1[1] + config.currDialogSize("y")/5))
+    elif player.colliderect(ruby):
+        if alpha < 120:
+            alpha += 30
+        dialog.blit(your_ruby, (textpos2[0], textpos2[1] + config.currDialogSize("y") / 5))
+    else:
+        if(alpha >= 0):
+            alpha -= 30
+
     window.blit(canvas, (0, 0))
+    window.blit(dialog, (0,config.currScreenSize()[1]-config.currDialogSize("y")))
     pygame.display.update()
     clock.tick(60)
 
--- config.py
+++ config.py
@@ -4,6 +4,7 @@ r = tk.Tk()
 ####screen
 screen_mode = "full" #full normal test
 SCREENSIZE = ((r.winfo_screenwidth(), r.winfo_screenheight()), (900,500),(400,400))
+dialogSize = ((r.winfo_screenwidth(), r.winfo_screenheight()/6), (900,500/6),(400,400/6))
 
 def currScreenSize():
     if screen_mode == "full":
@@ -14,6 +15,23 @@ def currScreenSize():
         return SCREENSIZE[2]
     else:
         print("Error screen_mode")
+
+def currDialogSize(axis):
+    if axis == "x":
+        if screen_mode == "full":
+            return dialogSize[0][0]
+        elif screen_mode == "normal":
+            return dialogSize[1][0]
+        elif screen_mode == "test":
+            return dialogSize[2][0]
+    if axis == "y":
+        if screen_mode == "full":
+            return dialogSize[0][1]
+        elif screen_mode == "normal":
+            return dialogSize[1][1]
+        elif screen_mode == "test":
+            return dialogSize[2][1]
+
 
 ####colors
 background = (5,20, 20)
